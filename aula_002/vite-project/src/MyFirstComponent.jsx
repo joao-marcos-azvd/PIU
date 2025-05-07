ccb03c4e5d681ee97f60d2bba25a7bee99e6e36f
JOÃO MARCOS DE AZEVEDO DANTAS <20221101110036@ifrn.local>
2025-05-07 17:49:52 -0300

    Aula de Iuri Redes, ele ensinou a usar React

---
+++ aula_002/vite-project/src/MyFirstComponent.jsx
@@ -0,0 +1,18 @@
+function MyFirstComponent () {
+   const nome = "João Marcos" 
+
+   return(
+        <>
+            <div>
+                <h1>Olá, mundo!!!</h1>
+            </div>
+            <div>
+                <p>Seu nome é: {nome}</p>
+                <p>A soma de 2+2 = {"Você sabe então faça de cabeça, seu bicho bruto mal educado!"}</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et esse vitae molestiae voluptatem accusamus corporis doloribus. Eveniet eligendi nostrum obcaecati minima est nobis cum atque nam! Nesciunt iure voluptates aspernatur.</p>
+        </>
+   )
+}
+
+export default MyFirstComponent
