f791407923d2c53cce4aaa11c776a987b503b003
JOÃO MARCOS DE AZEVEDO DANTAS <20221101110036@ifrn.local>
2025-05-22 08:02:33 -0300

    Não sei um nome

--- Meus_testes/teste_002 - Tentando fazer o desafio de Iuri/desafio_iuri/src/App.jsx
+++ Meus_testes/teste_002 - Tentando fazer o desafio de Iuri/desafio_iuri/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Mudar_tema from './components/Mudar_tema'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Mudar_tema />
     </>
   )
 }
--- Meus_testes/teste_002 - Tentando fazer o desafio de Iuri/desafio_iuri/src/components/Mudar_tema.jsx
+++ Meus_testes/teste_002 - Tentando fazer o desafio de Iuri/desafio_iuri/src/components/Mudar_tema.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react"
+
 export default function Mudar_tema() {
-    
+
+    const [tema, setTema] = useState(false)
+
+    function changecolormode(){
+        if (!tema) {
+            document.body.style.backgroundColor = "#101010"
+            document.body.style.color = "white"
+            setTema(true)
+        } else {
+            document.body.style.backgroundColor = "#fefefe"
+            document.body.style.color = "#101010"
+            setTema(false)
+        }
+    }
+
+    return (
+        <>
+            <button onClick={changecolormode}>Clique</button>
+        </>
+    )
 }
