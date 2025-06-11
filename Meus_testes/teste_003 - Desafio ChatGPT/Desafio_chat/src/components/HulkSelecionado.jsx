d73340e6705122ca77c699d1d7e878bc05094262
JOÃO MARCOS DE AZEVEDO DANTAS <20221101110036@ifrn.local>
2025-06-11 16:02:35 -0300

    Só fiz comentários mesmo

--- Meus_testes/teste_003 - Desafio ChatGPT/Desafio_chat/src/components/HulkSelecionado.jsx
+++ Meus_testes/teste_003 - Desafio ChatGPT/Desafio_chat/src/components/HulkSelecionado.jsx
@@ -1,13 +1,16 @@
-export default function HulkSelecionado({hulk_detalhes}) {
-    if (!hulk_detalhes) {
+export default function HulkSelecionado({hulk_detalhes}) { // Aqui eu crio o meu componente HulkSelecionado - Tem que ser passado um parametro para ele, que nesse caso vai ser uma lista com os dados 
+
+    if (!hulk_detalhes) { // Caso não tenha nem um hulk, ele retorna null
         return null;
     }
 
+    // Caso algum hulk tenha sido selecionado, ele retorna isso.
     return (
-        <div className="hulk_selecionado">
-            <h2>Detalhes do: {hulk_detalhes.nome}</h2>
-            <img src={hulk_detalhes.url} alt={hulk_detalhes.nome} />
-            <p>{hulk_detalhes.descricao}</p>
+        <div className="hulk_selecionado"> {/* Div de retorno */}
+            {/* Aqui eu uso a manipulação de listas */}
+            <h2>Detalhes do: {hulk_detalhes.nome}</h2> {/* Passo o nome como parametro */}
+            <img src={hulk_detalhes.url} alt={hulk_detalhes.nome} /> {/* Passo as informações para a tag imagem - url e alt */}
+            <p>{hulk_detalhes.descricao}</p> {/* Descrição */}
         </div>
     );
 }
